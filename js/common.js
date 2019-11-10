2887bc7d0b712025e56bf71df5a022981b91bb27
Van Thanh Le <vanthanhle@Vans-MacBook-Pro.local>
2019-11-10 09:45:47 +0100

    ad

--- js/common.js
+++ js/common.js
@@ -1182,8 +1182,8 @@ function getSpecificRoles(){
 
 function getCurrentDateTime(){
   var today = new Date();
-  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
+  var time = today.getHours() + ":" + today.getMinutes();// + ":" + today.getSeconds();
   var dateTime = date+' '+time;
   return {
     dateTime : dateTime
