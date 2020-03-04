bcb722827bb2a02b0403db6a52d3d3cb72909aab
Van Thanh Le <vanthanhle@Vans-MacBook-Pro.local>
2020-03-04 18:54:47 +0100

    A

--- js/listShipping.js
+++ js/listShipping.js
@@ -954,7 +954,7 @@ function showTask(){
         '<div class="card cardElement_'+e+'">'+
           '<div class="card-header" id="heading_"'+e+'>'+
             '<h5 class="mb-0">'+
-              '<button class="btn btn-link btnOrder_'+e+'" data-toggle="collapse" data-target="#collapse_'+e+'" aria-expanded="false" aria-controls="collapse_'+e+'">'+
+              '<button class="btn '+(lsTask[e][6]==1 ? "textRed" :"")+' btn-link btnOrder_'+e+'" data-toggle="collapse" data-target="#collapse_'+e+'" aria-expanded="false" aria-controls="collapse_'+e+'">'+
                 lsTask[e][0]+' | '+lsTask[e][1] + (lsTask[e][6]==1 ? "| PAID" : "")+
               '</button>'+
             '</h5>'+
