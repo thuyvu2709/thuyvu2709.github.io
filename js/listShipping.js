63c8fba556736e83e19d0e5281dfef3c3e89dc5e
Van Thanh Le <vanthanhle@Vans-MacBook-Pro.local>
2020-03-04 21:02:03 +0100

    A

--- js/listShipping.js
+++ js/listShipping.js
@@ -948,7 +948,7 @@ function showTask(){
       "Đã thanh toán: "+(lsTask[e][6] ? "Rồi" : "Chưa")+"<br/>";
       
       editTask = '<div class="btn btn-default btnNormal editTask order_'+e+'" style="margin:10px 10px 0;">Sửa thông tin</div>';
-      payTaskBtn = '<div class="btn btn-default btnNormal payTask order_'+e+'" style="margin:10px 10px 0;">Sửa thông tin</div>';
+      payTaskBtn = '<div class="btn btn-default btnNormal payTask order_'+e+'" style="margin:10px 10px 0;">Thanh toán ship</div>';
 
     }
     $("#listShippingOrder").append(
