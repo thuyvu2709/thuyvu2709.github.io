9cedf0ef5ef40449c334bb25d6876ab3a496000a
Van Thanh Le <vanthanhle@Vans-MacBook-Pro.local>
2020-03-21 14:09:46 +0100

    AD

--- js/listShipping.js
+++ js/listShipping.js
@@ -649,7 +649,7 @@ function startPreparingFn(){
     } else {
       console.log("prepare done")
       $(".modal-body").empty();
-      var content = "Xong ! Tổng cộng "+lsOrderDetail[lsOrder[orderIndex][0]].numOfProd+" cái, nhớ chụp ảnh hàng";
+      var content = "Xong ! Tổng cộng "+lsOrderDetail[lsOrder[orderIndex][0]].numOfProd+" cái, nhớ chụp 2 bức ảnh hàng";
       $(".modal-body").html(content);
     }
     $('.prepareNext').click(function(){
