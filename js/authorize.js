090f6855d5154202e4649b1522a47a40aea474b9
Van Thanh Le <vanthanhle@Vans-MacBook-Pro.local>
2020-07-20 17:32:10 +0200

    Ad

--- js/authorize.js
+++ js/authorize.js
@@ -224,8 +224,8 @@ function updateTitle(){
 
         var totalTurnover =  parseFloat(data[1][1]);
         var totalPay =  parseFloat(data[3][1]);
-        var totalPayForRestProduct = parseInt(data[5][1]);  
-        totalTurnover = totalTurnover - totalPayForRestProduct;
+        var totalRefund = parseInt(data[6][1]);
+        totalTurnover = totalTurnover - totalRefund;
         
         var breakevenPoint = parseFloat(totalPay - totalTurnover);
         if (breakevenPoint <=0) {
