7d29eee0b75e86957a2c3075a30c3a113ca3277d
Van Thanh Le <levanthanh3005@gmail.com>
2021-05-16 20:58:57 +0200

    AD

--- js/bsc.js
+++ js/bsc.js
@@ -1,6 +1,13 @@
 $("#headerInclude").load("../common/header.html");
 
-function loadWarehouse(callback) {
+var url = new URL(window.location.href);
+var filter = url.searchParams.get("filter");
+if (!filter) {
+  filter = "transaction"
+}
+console.log("filter:"+filter)
+
+function loadBSC(callback) {
   $.ajax({
     url: "https://bscaddress.herokuapp.com/lastupdate",
     success: function(res) {
@@ -22,15 +29,19 @@ $("#forceFetch").click(function(){
 
 $("#loadingSpin").show();
 
-loadWarehouse(function(response){
+loadBSC(function(response){
   $("#loadingSpin").hide();
   console.log("Gooo");
-  loadWarehouseHtml(response);
+  if (filter == "transaction") {
+    loadBSCTransaction(response);
+  } else {
+    loadBSCToken(response)
+  }
 })
 
 var data;
 
-function loadWarehouseHtml(response) {
+function loadBSCTransaction(response) {
   data = response.tx;
   var lastUpdate = response.last
   var timeAfterLast = (parseFloat(response.timeAfterLast) / (1000*60)).toFixed(1)
@@ -119,3 +130,55 @@ function fnEditAlertLower() {
     }
   });
 }
+
+
+function loadBSCToken(response) {
+  data = response.tokens;
+  var lastUpdate = response.last
+  var timeAfterLast = (parseFloat(response.timeAfterLast) / (1000*60)).toFixed(1)
+  $("#infor").html("Cập nhật lần cuối:"+timeAfterLast+" phút trước")
+  $("#listBSC").empty();
+  // console.log(data);
+  for(var e in data) {
+    
+
+    priceLastToUpdate = parseFloat(data[e].priceLastToUpdate) % 1000;
+
+    var cardBody = 
+    "<span>- USD lúc mua token:"+data[e].usdAmount+"</span></br>"+
+    "<span>- Giá token lúc mua cuối:"+parseFloat(data[e].tokenPriceAtBuyingTime).toFixed(15)+"</span></br>"+
+    "<span>- USD hiện tại:"+parseFloat(data[e].currentUSDAmount).toFixed(2)+"</span></br>"+
+    "<span>- Giá token hiện tại:"+parseFloat(data[e].tokenPriceNow).toFixed(15)+"</span></br>"+
+    "<span>- Số lượng token:"+data[e].tokenAmount+"</span></br>"+
+    "<span>- Địa chỉ contract:"+data[e].tokenAddress+"</span></br>"+
+    "<span>- Lần mua cuối:"+data[e].executionTime+"</span></br>"+
+    "<span>- USD lãi:"+parseFloat(data[e].gainUSD).toFixed(2)+"</span></br>"+
+    "<span>- USD % lãi:"+data[e].gainUSDRate+"</span></br>"+
+    "<span>- Cập nhật cuối:"+priceLastToUpdate+" s </span></br>"+
+    "<span><a href='https://poocoin.app/tokens/"+data[e].tokenAddress+"'>Xem chart</a></span>";
+
+    txLive = data[e].priceSource == "web3" ? "| Live" : "";
+
+    $("#listBSC").append(
+      // '<a href="#" class="list-group-item list-group-item-action orderelement order_'+e+'">'+data[e][0]+' | '+data[e][2]+' | '+data[e][5]+'</a>'
+      '<div class="card cardElement_'+e+'">'+
+        '<div class="card-header" id="heading_"'+e+'>'+
+          '<h5 class="mb-0">'+
+            '<button class="btn btn-link btnOrder_'+e+'" data-toggle="collapse" data-target="#collapse_'+e+'" aria-expanded="false" aria-controls="collapse_'+e+'">'+
+              data[e].tokenName+' | '+parseFloat(data[e].currentUSDAmount).toFixed(2) + ' USD | '+data[e].gainUSDRate+' % '+ txLive +
+            '</button>'+
+          '</h5>'+
+        '</div>'+
+
+        '<div id="collapse_'+e+'" class="collapse" aria-labelledby="heading_'+e+'" data-parent="#listBSC">'+
+          '<div class="card-body">'+
+            cardBody +
+          '</div>'+
+        '</div>'+
+      '</div>'
+      )
+  }
+  $(".editAlertUpper").click(fnEditAlertUpper)
+  $(".editAlertLower").click(fnEditAlertLower)
+
+};
