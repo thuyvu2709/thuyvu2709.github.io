35706d194a7845883b4205203226fcd9c1545ab6
root <root@slnb0015>
2024-02-15 12:54:34 +0100

    ad

--- js/listShipping.js
+++ js/listShipping.js
@@ -344,14 +344,77 @@ function loadOrderShippingListHtml() {
       }
     }
     
-
-    // if (mode == "Need_Schedule") {
-    //   if (lsOrder[e][11]) {
-    //     continue;
-    //   }
-    // }
-
     var shipIcon = '[<i class="fas fa-motorcycle">'+lsOrder[e][8]+'</i>]';
+
+    var mark = "";
+    var bgMarkAsLogiticCodice = ""
+    if (lsOrder[e][8] == "POST_COD" || lsOrder[e][8] == "POST_NO_COD" ){
+      if (lsOrderDetail[lsOrder[e][0]].otherInfor) {
+        try {
+          lsOrderDetail[lsOrder[e][0]].otherInfor = JSON.parse(lsOrderDetail[lsOrder[e][0]].otherInfor);
+        } catch(e) {
+
+        }
+        // console.log(lsOrderDetail[lsOrder[e][0]].otherInfor)
+
+        var logiticCodice = undefined
+        try {
+          logiticCodice = lsOrderDetail[lsOrder[e][0]].otherInfor.order.order.label;
+          if (logiticCodice){
+            orderDetailBrief+= "GHTK Code: <b><span class='textRed ghtkCode'>" + logiticCodice + "</span></b><br/>";
+            mark = "textGreen";
+            bgMarkAsLogiticCodice = "bgBisque"
+            shipIcon = '[<img src="../img/ghtk.png" class="shippingLogo">'+lsOrder[e][8]+'</img> ]';
+          }
+          // console.log(lsOrder[e][0]+" "+lsOrderDetail[lsOrder[e][0]].otherInfor.order.order.label);
+        } catch (e) {
+
+        }
+        
+        try {
+          logiticCodice = lsOrderDetail[lsOrder[e][0]].otherInfor.order.viettelPostlabel;
+          if (logiticCodice){
+            orderDetailBrief+= "ViettelPost Code: <b><span class='textRed ghtkCode'>" + logiticCodice + "</span></b><br/>";
+            mark = "textRed";
+            bgMarkAsLogiticCodice = "bgBisque"
+            shipIcon = '[<img src="../img/viettelpost.png" class="shippingLogo">'+lsOrder[e][8]+'</img> ]';
+          }
+          // console.log(lsOrder[e][0]+" "+lsOrderDetail[lsOrder[e][0]].otherInfor.order.order.label);
+        } catch (e) {
+
+        }
+
+        try {
+          logiticCodice = lsOrderDetail[lsOrder[e][0]].otherInfor.order.ghnLabel;
+          if (logiticCodice){
+            orderDetailBrief+= "GHN Code: <b><span class='textRed ghtkCode'>" + logiticCodice + "</span></b><br/>";
+            mark = "textRed";
+            bgMarkAsLogiticCodice = "bgBisque"
+            shipIcon = '[<img src="../img/ghn.png" class="shippingLogo">'+lsOrder[e][8]+'</img> ]';
+
+          }
+          // console.log(lsOrder[e][0]+" "+lsOrderDetail[lsOrder[e][0]].otherInfor.order.order.label);
+        } catch (e) {
+
+        }
+
+        if (!logiticCodice) {
+          // if (userRole=="manager"){
+            try {
+              savedGHTKRequest = lsOrderDetail[lsOrder[e][0]].otherInfor.savedRequest;
+              // console.log(savedGHTKRequest);
+              if (savedGHTKRequest) {
+                orderDetailBrief+= '<div class="btn btn-default btnNormal5px getGHTKCodeFromSavedRequest getGHTKCodeFromSavedRequest_'+e+'" >Lấy mã GHTK</div><br/><br/>';
+              }
+              // console.log(lsOrder[e][0]+" "+lsOrderDetail[lsOrder[e][0]].otherInfor.order.order.label);
+            } catch (e) {
+
+            }
+          // }
+        }
+      }
+    }
+
     var title = lsOrder[e][0]+' | '+lsOrderDetail[lsOrder[e][0]].customerName+" | "+lsOrder[e][1] +" | "+shipIcon;
 
 
@@ -520,71 +583,6 @@ function loadOrderShippingListHtml() {
 
     orderDetailBrief+=(lsOrderDetail[lsOrder[e][0]].orderNode ? "Note:"+lsOrderDetail[lsOrder[e][0]].orderNode+"<br/>" : "");
     orderDetailBrief+=(lsOrderDetail[lsOrder[e][0]].otherInfor ? lsOrderDetail[lsOrder[e][0]].otherInfor.isFreeShip==true ? "<span class='text-mustard'>Shop thanh toán ship, free ship cho khách</span><br/>" : "" : "")
-
-    var mark = "";
-    var bgMarkAsLogiticCodice = ""
-    if (lsOrder[e][8] == "POST_COD" || lsOrder[e][8] == "POST_NO_COD" ){
-      if (lsOrderDetail[lsOrder[e][0]].otherInfor) {
-        try {
-          lsOrderDetail[lsOrder[e][0]].otherInfor = JSON.parse(lsOrderDetail[lsOrder[e][0]].otherInfor);
-        } catch(e) {
-
-        }
-        // console.log(lsOrderDetail[lsOrder[e][0]].otherInfor)
-
-        var logiticCodice = undefined
-        try {
-          logiticCodice = lsOrderDetail[lsOrder[e][0]].otherInfor.order.order.label;
-          if (logiticCodice){
-            orderDetailBrief+= "GHTK Code: <b><span class='textRed ghtkCode'>" + logiticCodice + "</span></b><br/>";
-            mark = "textRed";
-            bgMarkAsLogiticCodice = "bgBisque"
-          }
-          // console.log(lsOrder[e][0]+" "+lsOrderDetail[lsOrder[e][0]].otherInfor.order.order.label);
-        } catch (e) {
-
-        }
-        
-        try {
-          logiticCodice = lsOrderDetail[lsOrder[e][0]].otherInfor.order.viettelPostlabel;
-          if (logiticCodice){
-            orderDetailBrief+= "ViettelPost Code: <b><span class='textRed ghtkCode'>" + logiticCodice + "</span></b><br/>";
-            mark = "textRed";
-            bgMarkAsLogiticCodice = "bgBisque"
-          }
-          // console.log(lsOrder[e][0]+" "+lsOrderDetail[lsOrder[e][0]].otherInfor.order.order.label);
-        } catch (e) {
-
-        }
-
-        try {
-          logiticCodice = lsOrderDetail[lsOrder[e][0]].otherInfor.order.ghnLabel;
-          if (logiticCodice){
-            orderDetailBrief+= "GHN Code: <b><span class='textRed ghtkCode'>" + logiticCodice + "</span></b><br/>";
-            mark = "textRed";
-            bgMarkAsLogiticCodice = "bgBisque"
-          }
-          // console.log(lsOrder[e][0]+" "+lsOrderDetail[lsOrder[e][0]].otherInfor.order.order.label);
-        } catch (e) {
-
-        }
-
-        if (!logiticCodice) {
-          // if (userRole=="manager"){
-            try {
-              savedGHTKRequest = lsOrderDetail[lsOrder[e][0]].otherInfor.savedRequest;
-              // console.log(savedGHTKRequest);
-              if (savedGHTKRequest) {
-                orderDetailBrief+= '<div class="btn btn-default btnNormal5px getGHTKCodeFromSavedRequest getGHTKCodeFromSavedRequest_'+e+'" >Lấy mã GHTK</div><br/><br/>';
-              }
-              // console.log(lsOrder[e][0]+" "+lsOrderDetail[lsOrder[e][0]].otherInfor.order.order.label);
-            } catch (e) {
-
-            }
-          // }
-        }
-      }
-    }
 
     orderDetailBrief+="<hr/>";
     var ghtkBtn = "";
