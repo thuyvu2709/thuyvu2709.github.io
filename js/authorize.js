fd84014b51f46c13dfbff35bacd138c7fdb4a1c7
root <root@slnb0015.>
2024-05-14 14:27:14 +0700

    ad

--- js/authorize.js
+++ js/authorize.js
@@ -15,11 +15,11 @@ var SCOPES = "https://www.googleapis.com/auth/spreadsheets "+
 var authorizeButton = document.getElementById('authorize_button');
 // var signoutButton = document.getElementById('signout_button');
 
-// var herokuPrefix = "https://cors-anywhere-2-c194.onrender.com/" // render need to go with https://console.cron-job.org/dashboard
+var herokuPrefix = "https://cors-anywhere-2-c194.onrender.com/" // render need to go with https://console.cron-job.org/dashboard
 // var herokuPrefix = "https://micro-cors.vercel.app/"
 // var herokuPrefix = "http://localhost:8080/"
 
-var herokuPrefix = "https://thingproxy.freeboard.io/fetch/" //https://nordicapis.com/10-free-to-use-cors-proxies/
+// var herokuPrefix = "https://thingproxy.freeboard.io/fetch/" //https://nordicapis.com/10-free-to-use-cors-proxies/
 
 var dataset = [];
 
